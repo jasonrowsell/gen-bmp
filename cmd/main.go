546c700ffa9eb1683b277159cd057efed96d4105
jasonrowsell <jasonrowsell@hotmail.co.uk>
2024-07-09 16:20:39 +0100

    feat: Add Colors property

--- cmd/main.go
+++ cmd/main.go
@@ -1,14 +1,23 @@
 package main
 
+type Color struct {
+	R, G, B float32
+}
+
 type Image struct {
 	Height int
 	Width  int
+	Colors [][]Color
 }
 
 func NewImage(h, w int) *Image {
 	img := &Image{
 		Height: h,
 		Width:  w,
+		Colors: make([][]Color, h),
+	}
+	for i := range img.Colors {
+		img.Colors[i] = make([]Color, w)
 	}
 
 	return img
--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,11 +1,23 @@
 package main
 
-import "testing"
+import (
+	"testing"
+)
 
 func TestNewImage(t *testing.T) {
 	img := NewImage(5, 10)
 
 	if img.Height != 5 || img.Width != 10 {
 		t.Errorf("Expected dimensions 5x10, got %dx%d", img.Height, img.Width)
+	}
+
+	if len(img.Colors) != 5 {
+		t.Errorf("Expected total Colors rows %d, got %d", 5, len(img.Colors))
+	}
+
+	for i := range img.Height {
+		if len(img.Colors[i]) != 10 {
+			t.Errorf("Expected Colors columns %d, got %d", 10, len(img.Colors[i]))
+		}
 	}
 }
