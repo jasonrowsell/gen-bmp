0df9b2282dd5087078a1d7df473f9183baf58f6f
jasonrowsell <jasonrowsell@hotmail.co.uk>
2024-09-20 12:09:19 +0100

    test: Test concurrency, boundaries, export

--- cmd/main_test.go
+++ cmd/main_test.go
@@ -1,6 +1,8 @@
 package main
 
 import (
+	"os"
+	"sync"
 	"testing"
 )
 
@@ -20,26 +22,77 @@ func TestNewImage(t *testing.T) {
 			t.Errorf("Expected Colors columns %d, got %d", 10, len(img.Colors[i]))
 		}
 	}
+}
 
-	c := Color{R: 0.4, G: 0.5, B: 0.6}
-	err := img.SetColor(c, 9, 4)
+func TestBoundaryConditions(t *testing.T) {
+	img := NewImage(5, 5)
+	c := Color{R: 1, G: 0, B: 0}
+
+	err := img.SetColor(c, 0, 0)
 	if err != nil {
-		t.Errorf("Unexpected error setting color: %v", err)
-	}
-	retrieved, err := img.GetColor(9, 4)
-	if err != nil {
-		t.Errorf("Unexpected error getting color: %v", err)
-	}
-	if retrieved != c {
-		t.Errorf("Expected color %v, got %v", c, retrieved)
+		t.Errorf("Unexpected error setting color at (0, 0): %v", err)
 	}
 
-	err = img.SetColor(c, 10, 5)
-	if err == nil {
-		t.Errorf("Expected error for out-of-bounds set, got nil")
+	_, err = img.GetColor(4, 4)
+	if err != nil {
+		t.Errorf("Unexpected error getting color at (4, 4): %v", err)
 	}
-	_, err = img.GetColor(10, 5)
+
+	err = img.SetColor(c, -1, 0)
 	if err == nil {
-		t.Errorf("Expected error for out-of-bounds get, got nil")
+		t.Error("Expected error for out-of-bounds, but got nil")
+	}
+
+	_, err = img.GetColor(5, 5)
+	if err == nil {
+		t.Error("Expected error for out-of-bounds, but got nil")
+	}
+}
+
+func TestConcurrentAccess(t *testing.T) {
+	img := NewImage(10, 5)
+	var wg sync.WaitGroup
+	c := Color{R: 0.5, G: 0.6, B: 0.7}
+
+	for y := 0; y < img.Height; y++ {
+		for x := 0; x < img.Width; x++ {
+			wg.Add(1)
+			go func(x, y int) {
+				defer wg.Done()
+				err := img.SetColor(c, y, x)
+				if err != nil {
+					t.Errorf("Unexpected error setting color: %v", err)
+				}
+				retrieved, err := img.GetColor(y, x)
+				if err != nil {
+					t.Errorf("Unexpected error getting color: %v", err)
+				}
+				if retrieved != c {
+					t.Errorf("Expected color %v, got %v", c, retrieved)
+				}
+			}(x, y)
+		}
+	}
+	wg.Wait()
+}
+
+func TestExport(t *testing.T) {
+	img := NewImage(10, 5)
+	color := Color{R: 0.5, G: 0.5, B: 0.5}
+	for y := 0; y < img.Height; y++ {
+		for x := 0; x < img.Width; x++ {
+			img.SetColor(color, y, x)
+		}
+	}
+
+	tmpFile := "test_image.bmp"
+	err := img.Export(tmpFile)
+	if err != nil {
+		t.Errorf("Unexpected error exporting image: %v", err)
+	}
+	defer os.Remove(tmpFile)
+
+	if _, err := os.Stat(tmpFile); os.IsNotExist(err) {
+		t.Errorf("Expected file %s to exist, but it does not", tmpFile)
 	}
 }
